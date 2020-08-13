af7ef40be2030a0c2b739804c7a1a110a9421b94
Jsquad <lakis006@umn.edu>
2020-08-13 18:54:03 -0500

    added to heroku

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
+import Search from "./Search";
+import "./styles.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App() {
+  return (
+    <div className="App">
+      <Search />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
