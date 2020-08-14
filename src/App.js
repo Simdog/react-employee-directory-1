3b0a5b27235e69f5d458521d9f6a4f664302ef7f
Jsquad <lakis006@umn.edu>
2020-08-13 21:07:53 -0500

    added the Employees and Header components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import Header from './Headerjs'
+import Header from './Header';
+import Employees from './Employees';
 import './App.css';
+
 
 class App extends Component {
   render() {
   return (
     <div className="App">
       <Header />
+      <Employees /> 
     </div>
   );
 }
