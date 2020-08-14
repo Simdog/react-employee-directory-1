5b25fb47f9b57263b30aef599b7cebdca4233b93
Jsquad <lakis006@umn.edu>
2020-08-13 20:51:31 -0500

    added a header after restarting on the app

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import App from "./App";
+
+const Header = () => {
+    return(
+        <header>
+            <h1> Employee Cards </h1>
+        </header>
+    )
+}
+
+export default App;
